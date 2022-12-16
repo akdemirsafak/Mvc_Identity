c1cda29f1a562ec87642ff1f241c1ce79fe22167
akdemirsafak <akdemirsafak@gmail.com>
2022-12-16 15:14:13 +0300

    ModelStateExtension

---
+++ IdentityMvc/Extensions/ModelStateExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace IdentityMvc.Extensions;
+
+public static class ModelStateExtensions
+{
+    public static void AddModelErrorList(this ModelStateDictionary modelState, List<string> errorMessages)
+    {
+        errorMessages.ForEach(x =>
+        {
+            modelState.AddModelError(string.Empty,x);
+        });
+    }
+}
